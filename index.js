60bb1c2bcd19b51589ff5e7cc39ce98704df6ea8
felix9ia <felixjiawork@gmail.com>
2020-11-04 14:47:20 +0800

    add estraverse

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const utils = require('./dist/main')
 var program = 'const answer = 42';
-console.log(utils);
-utils.demo1();
+// utils.demo1();
 var success = utils.esprima.tokenize(program);
-console.log(success);
+// console.log(success);
+
+console.log(utils.estraverse);
+
+
+
 
--- src/utils.js
+++ src/utils.js
@@ -1,5 +1,5 @@
 var esprima = require('esprima');
-// var estraverse = require('estraverse')
+var estraverse = require('estraverse')
 
 var demo1 = function demo1(params) {
     console.log("hello, im demo1");
@@ -7,7 +7,8 @@ var demo1 = function demo1(params) {
 
 var utils = {
     esprima:esprima,
-    demo1: demo1
+    demo1: demo1,
+    estraverse
 }
 
 module.exports = utils
