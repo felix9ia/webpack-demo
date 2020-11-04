de9ce63cb791aa511db8abba633094f0e17443ea
felix9ia <felixjiawork@gmail.com>
2020-11-04 14:21:07 +0800

    add demo

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const utils = require('./dist/main')
-
+var program = 'const answer = 42';
 console.log(utils);
 utils.demo1();
+var success = utils.esprima.tokenize(program);
+console.log(success);
 
